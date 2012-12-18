31fd72b5d4b25c397f013d9ff40993044b982ce5
Svante Seleborg <svante@axantum.com>
2012-12-18 12:51:27 +0100

    Update to work with building of AxSigTools

---
+++ AxSigLib/Base34Enc.h
@@ -0,0 +1,41 @@
+#ifndef AXSIGLIB_BASE34ENC_H
+#define AXSIGLIB_BASE34ENC_H
+/*! \file
+    \brief AxSigLib - Short Elliptic Curve Digital Signature Algorithm et. al.
+
+    @(#) $Id$
+
+    Encode Base34 strings. These are strings using A-N, P-Z, 1-9 as their digits,
+    valued in that sequence, i.e. A is zero, 9 is thirty-two.
+
+    Copyright (C) 2005 Svante Seleborg/Axon Data, All rights reserved.
+
+    The author may be reached at mailto:axcrypt@axondata.se and http://axcrypt.sourceforge.net
+----
+*/
+
+#include "Base34.h"
+#include "BaseMEnc.h"
+
+//! Converts given data to base 34 (a-z, 1-9, excluding the letter 'o' and the digit '0').
+class Base34Encoder : public SimpleProxyFilter {
+public:
+    Base34Encoder(BufferedTransformation *attachment = NULL, int bits = 0, bool uppercase = true, int outputGroupSize = 0, const std::string &separator = ":", const std::string &terminator = "")
+        : SimpleProxyFilter(new BaseM_Encoder(new Grouper), attachment) {
+        IsolatedInitialize(MakeParameters(Name::Uppercase(), uppercase)
+                                         (Name::GroupSize(), outputGroupSize)
+                                         (Name::BitPrecision(), bits)
+                                         (Name::Separator(), ConstByteArrayParameter(separator)));
+    }
+
+    void IsolatedInitialize(const NameValuePairs &parameters);
+};
+
+void Base34Encoder::IsolatedInitialize(const NameValuePairs &parameters) {
+    bool uppercase = parameters.GetValueWithDefault(Name::Uppercase(), true);
+    m_filter->Initialize(CombinedNameValuePairs(
+        parameters,
+        MakeParameters(Name::EncodingLookupArray(), uppercase ? &s_vecUpper[0] : &s_vecLower[0], false)(Name::AnyBase(), 34, true)));
+}
+
+#endif
---
+++ AxSigLib/SSign.h
@@ -0,0 +1,68 @@
+#ifndef AXSIGLIB_SECDSA_SSIGN_H
+#define AXSIGLIB_SECDSA_SSIGN_H
+/*! \file
+    \brief AxSigLib - Short Elliptic Curve Digital Signature Algorithm et. al.
+
+    @(#) $Id$
+
+    The SECDSA Signer
+
+    Copyright (C) 2001 Svante Seleborg/Axantum Software AB, All rights reserved.
+
+    The author may be reached at mailto:axcrypt@axondata.se and http://axcrypt.sourceforge.net
+---
+*/
+
+#include "secdsa.h"
+
+template <class SSS> class SHORTSIGN : public SSS::Signer, public Canonicalize {
+public:
+    SHORTSIGN() : SSS::Signer() {}
+    SHORTSIGN(BufferedTransformation &bt) : SSS::Signer(bt) {}
+
+    /// \brief Calculate actual signature length
+    /// There is a potential problem here... If this is used inside the class for purposes
+    /// of memory allocation, we're in trouble as it's virtual. Not sure if it really should
+    /// be, but that's the way it is - and it does appear that currently it's not used
+    /// from base classes for that purpose. Presumably MaxSignatureLength() serves that
+    /// purpose, but the documentation for the library somewhat brief. (Sarcasm intended).
+    virtual unsigned int SignatureLength() const {
+        return ((SSS::Rbits()-1) / 8) + 1
+            + GetSignatureAlgorithm().SLen(this->GetAbstractGroupParameters());
+    }
+
+    //! maximum signature length produced for a given length of recoverable message part
+    virtual unsigned int MaxSignatureLength(unsigned int recoverablePartLength = 0) const {
+        return max(SignatureLength(), SSS::Signer::MaxSignatureLength(recoverablePartLength));
+    }
+
+    unsigned int SignAndRestart(RandomNumberGenerator &rng, PK_MessageAccumulator &messageAccumulator, byte *signature, bool restart) const {
+        // Allocate a temp buffer for the inner signature
+        SecByteBlock largesig(SSS::Signer::SignatureLength());
+
+        // Do the inner signature, the siglen returned is our own siglen since SignatureLength() is virtual...
+        unsigned int siglen = SSS::Signer::SignAndRestart(rng, messageAccumulator, largesig, restart);
+
+        unsigned int rLen = GetSignatureAlgorithm().RLen(this->GetAbstractGroupParameters());
+        unsigned int sLen = GetSignatureAlgorithm().SLen(this->GetAbstractGroupParameters());
+
+        // Unpack r and s again into Integer's.
+        Integer r, s;
+        r.Decode(largesig, rLen);
+        s.Decode(largesig + rLen, sLen);
+
+        // Now prepare a compact representation, where we shorten r to RBITS, and the shift it
+        // up just above r in one compact Integer.
+
+        Integer sr = r;
+
+        sr += s << SSS::Rbits();
+        sr.Encode(signature, SignatureLength());
+
+        //this->GetAbstractGroupParameters().GetGroupOrder().BitCount()
+        //memcpy(signature, largesig, siglen);
+        return siglen;
+    };
+};
+
+#endif
