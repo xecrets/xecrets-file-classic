49811fc07fc8023e6bfbc9a7b6d5b1b9a8c086ae
Svante Seleborg <svante.seleborg@axcrypt.net>
2019-11-01 21:56:40 +0100

    Upgrade Crypto++ to 8.2.0 #1

--- Build.cmd
+++ Build.cmd
@@ -1,6 +1,6 @@
-call "%VSINSTALLDIR%vc\vcvarsall" x86
+call "%VSINSTALLDIR%vc\Auxiliary\Build\vcvarsall" x86
 MSBuild AxCrypt.sln /p:Configuration=Debug;Platform=Win32
 MSBuild AxCrypt.sln /p:Configuration=Release;Platform=Win32
-call "%VSINSTALLDIR%vc\vcvarsall" amd64
+call "%VSINSTALLDIR%vc\Auxiliary\Build\vcvarsall" amd64
 MSBuild AxCrypt.sln /p:Configuration=Debug;Platform=x64
 MSBuild AxCrypt.sln /p:Configuration=Release;Platform=x64
